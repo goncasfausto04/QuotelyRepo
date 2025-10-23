324bff2991a9b25078e98c78647b059bc91a801c
goncasfausto04 <20231721@novaims.unl.pt>
2025-10-23 10:41:13 +0100

    .env

--- quotely-app/src/supabaseClient.js
+++ quotely-app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ncxchpmtfjbejciucken.supabase.co";
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeGNocG10ZmpiZWpjaXVja2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzQxNTEsImV4cCI6MjA3NjIxMDE1MX0.9rFxNW-1s-KHfQMMYHOWMN2SjvPln6Xkr4S5vts3tks";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
